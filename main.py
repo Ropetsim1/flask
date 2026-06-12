f36dcab9f5879c05adb04a5bedd1d348c0aea909
ropetsim1 <roberts.lodins@gmail.com>
2026-06-12 20:06:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -65,5 +65,6 @@ def createaccountapi():
     save_data(data)
 
     return f"Created account: {user.accountname} ur secret key is {secretkey}"
-if __name__ == "__main__":
-    app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
